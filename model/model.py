88a5f3ae7853e07671511b1189450a093b25c32c
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-01-25 13:07:48 -0500

    Inicio, punto 1

---
+++ model/model.py
@@ -0,0 +1,20 @@
+import math
+
+class Functions:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def circle_radius(radius):
+        """
+            Calculates the circle area
+            :param radius: the radius of a circle
+            :return: the circle's area
+        """
+        return math.pi * radius**2
+
+
+
+
+
