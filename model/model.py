c1de87eed72938671bee67877d0148050cbd99ec
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-01-25 13:47:00 -0500

    Más funciones

--- model/model.py
+++ model/model.py
@@ -68,11 +68,33 @@ class Functions:
         """
             7 - Return tghe verification of a number
             :param num: The number to be verified
-            :return:
         """
-    
+        if num < 100:
+            return "El número es menor que 100"
+        elif num in range(100, 1001):
+            return "El numero esta entre 100 y 1000"
+        elif num in range(1000, 2001):
+            return "El numero está entre 1000 y 2000"
+        elif num > 2000:
+            return "El numero es mayor a 2000"
 
+    @staticmethod
+    def pair_verification(num):
+        """
+            8 - Verifies if a number is an odd or and even number
+            :param num: The number to be verified
+        """
+        if num % 2 == 0:
+            return "The number is even"
+        else:
+            return "the number is odd"
 
+    @staticmethod
+    def num_in_array(num):
+        # Pendiente
+        pass
 
-
-
+    @staticmethod
+    def last_n_numbers(num):
+        """  """
+        
